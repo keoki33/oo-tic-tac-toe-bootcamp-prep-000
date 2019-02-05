5b834b3d78d89d8d92754277e14b094c40c3e912
George Wong <gwong323@yahoo.com>
2019-02-05 17:06:05 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -38,7 +38,7 @@ def turn
   puts "Select a position between 1-9."
   input = gets.strip
   index = input_to_index(input)
-  if valid_move(index)
+  if valid_move?(index)
     move(index, current_player)
     display_board
   else
